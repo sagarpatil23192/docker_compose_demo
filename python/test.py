71b65a2f4c8de762bd4c6cf9d38504a489b2959f
root <root@ip-172-31-42-23.ec2.internal>
2017-07-19 09:49:46 +0000

    latest

--- python/test.py
+++ python/test.py
@@ -3,7 +3,7 @@
 import MySQLdb
 import time
 
-time.sleep(20)
+time.sleep(10)
 
 # Open database connection
 db = MySQLdb.connect("db","root","root","domo_mom_db")
@@ -11,12 +11,23 @@ db = MySQLdb.connect("db","root","root","domo_mom_db")
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-sql = "INSERT INTO directory (email, phone_number) values ('sagar@gmail.com', '9595504298')"
+tweets = open("keywords.txt", "w")
+
+sql = "SELECT * FROM directory"
 try:
    # Execute the SQL command
    cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[1]
+      print>>tweets, fname
+      # Now print fetched result
+      print "fname=%s" % \
+             (fname)
 except:
    print "Error: unable to fecth data"
 
 # disconnect from server
 db.close()
+
